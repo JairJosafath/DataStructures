43fadc93a7b4ec58d2736778b1e7c941ca87f360
Jair <Jairjosafath@gmail.com>
2023-01-07 01:00:58 -0500

    start Graphs with AL

---
+++ Graph/Graph.h
@@ -0,0 +1,10 @@
+//
+// Created by jairj on 1/7/2023.
+//
+
+#ifndef DATASTRUCTURES_GRAPH_H
+#define DATASTRUCTURES_GRAPH_H
+
+//using the LL I created I will make a graph using the adjecency list sepresentation
+
+#endif //DATASTRUCTURES_GRAPH_H
